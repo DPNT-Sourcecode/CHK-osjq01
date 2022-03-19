ce2853e0e5a3250a38c4912281da9850bcc7a1b6
sbx_user1051 <sbx_user1051@169.254.2.237>
2022-03-19 20:54:07 +0000

    Sat Mar 19 20:54:07 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,8 +3,17 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    costs = {'A':50, 'B':30, 'C':20, 'D':15}
     dict = {}
     for c in skus:
-        dict[]    
+        dict[c]+=1
+
+    # Calculate cost
+    total = 0
+    for product in ['A', 'B','C', 'D']:
+        total += dict[product] *costs[product]
+
+    return total
+
 
 
